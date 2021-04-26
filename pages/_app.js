567ce5e7f208c1bb3b5b29e697a06e7d3d995d26
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-04-26 13:59:14 +0200

    Initial setup of _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { customTheme } from "../src/theme";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MuiThemeProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </MuiThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
