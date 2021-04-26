584c45fdc7e2fe5304b4918d86a1c9ff2753a293
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-04-26 21:37:17 +0200

    Theme with breakpoints

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,13 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-export const customTheme = createMuiTheme({});
+export const customTheme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
