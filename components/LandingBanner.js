f8a5417a69fde5895ffbdb41c14d3dfb5a029be5
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-05-03 08:18:24 +0200

    Started working on home page

---
+++ components/LandingBanner.js
@@ -0,0 +1,11 @@
+import { useStyles } from "../styles/LandingBannerStyles";
+
+export default function LandingBanner() {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.bannerContainer}>
+      <div></div>
+    </div>
+  );
+}
---
+++ styles/LandingBannerStyles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  bannerContainer: {
+    width: "100%",
+    height: "100%",
+    background: "linear-gradient(180deg, #141433 25.52%, #1c1c39 71.87%, #20203d 93.23%, #2c2c47 100%)",
+  },
+}));
