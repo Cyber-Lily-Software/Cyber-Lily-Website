ea58ac579202191ae1413062e9dfe5ee5e6081fb
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-05-07 11:50:38 +0200

    Working on homepage banner

--- components/LandingBanner.js
+++ components/LandingBanner.js
@@ -1,11 +1,30 @@
 import { useStyles } from "../styles/LandingBannerStyles";
 
+import Button from "@material-ui/core/Button";
+
 export default function LandingBanner() {
   const styles = useStyles();
 
   return (
-    <div className={styles.bannerContainer}>
-      <div></div>
+    <div className={styles.banner}>
+      <div className={styles.bannerContent}>
+        <div className={styles.bannerText}>
+          <h1>Simple Solutions to Complex Problems</h1>
+          <p>We design and develop amazing web products that grow your business.</p>
+        </div>
+        <div className={styles.bannerImage}>
+          <picture>
+            <source media="(min-width: 2560px)" srcset="/home_page/banner_home@2x.png" />
+            <source media="(min-width: 1440px)" srcset="/home_page/banner_home.png" />
+            <source media="(min-width: 1024px)" srcset="/home_page/banner_mobile.png" />
+            <source media="(min-width: 768px)" srcset="/home_page/banner_tablet.png" />
+            <img src="/home_page/banner_mobile.png" />
+          </picture>
+        </div>
+        <div className={styles.bannerContactButton}>
+          <Button>Get in touch</Button>
+        </div>
+      </div>
     </div>
   );
 }
--- styles/HomePageStyles.js
+++ styles/HomePageStyles.js
@@ -4,5 +4,8 @@ export const homePageStyles = makeStyles((theme) => ({
   root: {
     width: "100vw",
     height: "100vh",
+    [theme.breakpoints.down(435)]: {
+      height: "70vh",
+    },
   },
 }));
