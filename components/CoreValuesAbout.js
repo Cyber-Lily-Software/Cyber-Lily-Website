3235178ee67c19acd64576dd5290a883f599ee04
David MacIntosh <macintoshdg4@gmail.com>
2021-05-13 12:26:15 +0200

    Core values component pretty much done

---
+++ components/CoreValuesAbout.js
@@ -0,0 +1,133 @@
+import Link from "next/link";
+
+import CoreValuesCard from "../components/CoreValuesCard";
+
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "center",
+    backgroundColor: "#F9F9FA",
+    paddingLeft: "7.5rem",
+    [theme.breakpoints.down(1025)]: {
+      paddingLeft: "1rem",
+      textAlign: "center !important",
+      alignContent: "center !important",
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  valueItem: {
+    width: "33%",
+    paddingBottom: "1rem",
+    [theme.breakpoints.down(1025)]: {
+      width: "100%",
+      textAlign: "center",
+      alignContent: "center",
+      marginLeft: "auto",
+      marginRight: "auto",
+      paddingLeft: "4%",
+    },
+    [theme.breakpoints.down(420)]: {
+      paddingLeft: "0%",
+    }
+  },
+  topText: {
+      width: "100%",
+      alignItems: "center",
+      fontWeight: 700,
+      fontSize: "2.5rem",
+      lineHeight: "1.5rem",
+      marginBottom: "4rem !important",
+      marginTop: "5rem !important",
+      textAlign: "left",
+      [theme.breakpoints.down(1025)]: {
+        textAlign: "center",
+      },
+      
+  },
+  line1: {
+    backgroundImage: "linear-gradient(120deg, #F68320 0%, #F68320 100%)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100% 0.2em",
+    backgroundPosition: "0 88%",
+    transition: "background-size 0.25s ease-in",
+  },
+}));
+
+const values = [
+  {
+    valueName: "Integrity",
+    avatar: "a1",
+    valueIcon1: "./shield-verified.png",
+    valueIcon2: "./shield-verified@2x.png",
+    valueIcon3: "./shield-verified@3x.png",
+    shortDesc:
+      "We design and develop custom websites from scratch, tailored specifically to your business’s needs. We pride ourselves on never using any templates for the design of our websites. We organize hosting for your websites through Amazon Web Services.",
+  },
+  {
+    valueName: "Motivation",
+    avatar: "a2",
+    valueIcon1: "./graph_increase.png",
+    valueIcon2: "./graph_increase@2x.png",
+    valueIcon3: "./graph_increase@3x.png",
+    shortDesc:
+      "We design and develop custom websites from scratch, tailored specifically to your business’s needs. We pride ourselves on never using any templates for the design of our websites. We organize hosting for your websites through Amazon Web Services.",
+  },
+  {
+    valueName: "Creative Openness",
+    avatar: "a3",
+    valueIcon1: "./lightbulb-big.png",
+    valueIcon2: "./lightbulb-big@2x.png",
+    valueIcon3: "./lightbulb-big@3x.png",
+    shortDesc:
+      "We design and develop custom websites from scratch, tailored specifically to your business’s needs. We pride ourselves on never using any templates for the design of our websites. We organize hosting for your websites through Amazon Web Services.",
+  },
+];
+
+export default function HomeServiceCards(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const mobileBreakpoint = useMediaQuery(theme.breakpoints.down(1025));
+
+  return (
+    <div className={classes.root}>
+        <h2 className={classes.topText} >Our <span className={classes.line1}>core values</span></h2>
+        {values.map((value) => {
+            if (mobileBreakpoint) {
+            return (
+                <div className={classes.valueItem}>
+                  <CoreValuesCard
+                      valueName={value.valueName}
+                      avatar={value.avatar}
+                      valueIcon={value.valueIcon}
+                      shortDesc={value.shortDesc}
+                      valueIcon1={value.valueIcon1}
+                      valueIcon2={value.valueIcon2}
+                      valueIcon3={value.valueIcon3}
+                  />
+                </div>
+            );
+            } else {
+            return (
+                <div className={classes.valueItem}>
+                    <CoreValuesCard
+                    valueName={value.valueName}
+                    avatar={value.avatar}
+                    valueIcon1={value.valueIcon1}
+                    valueIcon2={value.valueIcon2}
+                    valueIcon3={value.valueIcon3}
+                    shortDesc={value.shortDesc}
+                    />
+                </div>
+            );
+            }
+        })}
+    </div>
+  );
+}
---
+++ components/CoreValuesCard.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Avatar from "@material-ui/core/Avatar";
+
+import clsx from "clsx";
+
+import { makeStyles, useTheme, withStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+        root: {
+            display: "flex",
+            textAlign: "center",
+            perspective: "40rem",
+            maxWidth: "70%",
+            [theme.breakpoints.down(1025)]: {
+                maxWidth: "90%",
+            },
+        },
+        innerCard: {
+            display: "flex",
+            textAlign: "center",
+        },
+        avatar: {
+            backgroundColor: "#E6F3E9",
+            position: "absolute",
+            [theme.breakpoints.down(1025)]: {
+                position: "relative",
+                marginRight: "-4rem",
+                marginLeft: "2.5rem",
+            }
+            
+        },
+        avatar2: {
+            backgroundColor: "#F1EBF2",
+            position: "absolute",
+            [theme.breakpoints.down(1025)]: {
+                position: "relative",
+                marginRight: "-3.5rem",
+                marginLeft: "2.5rem",
+            },
+            
+        },
+        avatar3: {
+            backgroundColor: "#FEF3E9",
+            position: "absolute",
+            [theme.breakpoints.down(1025)]: {
+                position: "relative",
+                marginRight: "-3rem",
+                marginLeft: "2.5rem",
+            },
+            [theme.breakpoints.down(769)]: {
+                position: "relative",
+                marginRight: "-3.5rem",
+                marginLeft: "2.5rem",
+            },
+            [theme.breakpoints.down(420)]: {
+                position: "relative",
+                marginRight: "-0.5rem",
+                marginLeft: "0rem",
+            }
+        },
+        descText: {
+            textAlign: "left",
+            fontWeight: 400,
+            fontSize: "1rem",
+            lineHeight: "1.5rem",
+            paddingTop: "0rem",
+            marginTop: "1.25rem",
+            [theme.breakpoints.down(1025)]: {
+                marginTop: "-2.5rem",
+                textAlign: "center",
+            },
+        },
+        title: {
+            textAlign: "left",
+            fontWeight: 500,
+            fontSize: "2rem",
+            lineHeight: "1.5rem",
+            paddingTop: "1rem",
+            marginBottom: "0rem !important",
+            [theme.breakpoints.down(1025)]: {
+                textAlign: "center",
+            },
+        },
+    }));
+
+    export default function CoreValuesCard(props) {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    function getAvatar(avatar) {
+        if (avatar === "a1") {
+          return <Avatar className={classes.avatar}>
+                    <picture>
+                        <source media="(min-width: 374px)" srcSet={props.valueIcon1} />
+                        <source media="(min-width: 1025px)" srcSet={props.valueIcon2} />
+                        <source media="(min-width: 2560px)" srcSet={props.valueIcon3} />
+                        <img src={props.valueIcon1} />
+                    </picture>
+                </Avatar>;
+        }
+        if (avatar === "a2") {
+            return <Avatar className={classes.avatar2}>
+                      <picture>
+                          <source media="(min-width: 374px)" srcSet={props.valueIcon1} />
+                          <source media="(min-width: 1025px)" srcSet={props.valueIcon2} />
+                          <source media="(min-width: 2560px)" srcSet={props.valueIcon3} />
+                          <img src={props.valueIcon1} />
+                      </picture>
+                  </Avatar>;
+          }
+          return <Avatar className={classes.avatar3}>
+                    <picture>
+                        <source media="(min-width: 374px)" srcSet={props.valueIcon1} />
+                        <source media="(min-width: 1025px)" srcSet={props.valueIcon2} />
+                        <source media="(min-width: 2560px)" srcSet={props.valueIcon3} />
+                        <img src={props.valueIcon1} />
+                    </picture>
+                </Avatar>;
+      }
+
+    const mobileBreakpoint = useMediaQuery(theme.breakpoints.down(4000));
+    return (
+        <div className={classes.root}>
+            {getAvatar(props.avatar)}
+            <div className={classes.descText}>
+                <p className={classes.title}>{props.valueName}</p>
+                <p>{props.shortDesc}</p>
+            </div>
+        </div>
+    );
+  }
